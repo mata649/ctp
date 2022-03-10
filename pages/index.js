9ad58d1f5626d1aa42987977efb0a0e2ba879a3e
Jose <mata649@hotmail.com>
2022-03-10 14:41:46 -0600

    s

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect, useState } from "react/cjs/react.production.min";
 import { Carousel } from "../components/general/Carousel";
 
 const Index = () => {
@@ -7,14 +8,22 @@ const Index = () => {
     "https://images.unsplash.com/photo-1523438097201-512ae7d59c44?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
     "https://images.unsplash.com/photo-1513026705753-bc3fffca8bf4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
   ];
+  const [imagenes, setImagenes] = useState(["1", "2", "3"]);
+
+  useEffect(() => {
+    const imagenes_getted = fetch("https://bakcend.com/images_carousel");
+    setImagenes(imagenes_getted);
+  }, []);
+
   return (
     <div className="container">
       <h1 className="text-center mt-4 ">
         Bienvenidos al Colegio Técnico Profesional de Cañas
       </h1>
       <Carousel images={images} />
+   
       <section className="mt-5 text-center fs-4 mx-5">
-        <h1 >Misión</h1>
+        <h1>Misión</h1>
         <p>
           Ser una institución reconocida a nivel regional, nacional e
           internacional, como la rectora del sistema educativo regional mediante
